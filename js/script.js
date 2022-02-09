b18b09aa4f0a44678057cb041cc53e636bbfa0bf
Aakash <srivastavaak1203@gmail.com>
2022-02-09 19:29:10 +0530

    add cart page

--- js/script.js
+++ js/script.js
@@ -12,17 +12,17 @@ if (close) {
     })
 }
 
-// var mainImg = document.getElementById("mainImg");
-// var sImg = document.getElementsByClassName("small-img");
-// sImg[0].onclick = function () {
-//     mainImg.src = sImg[0].src;
-// }
-// sImg[1].onclick = function () {
-//     mainImg.src = sImg[1].src;
-// }
-// sImg[2].onclick = function () {
-//     mainImg.src = sImg[2].src;
-// }
-// sImg[3].onclick = function () {
-//     mainImg.src = sImg[3].src;
-// }
+var mainImg = document.getElementById("mainImg");
+var sImg = document.getElementsByClassName("small-img");
+sImg[0].onclick = function () {
+    mainImg.src = sImg[0].src;
+}
+sImg[1].onclick = function () {
+    mainImg.src = sImg[1].src;
+}
+sImg[2].onclick = function () {
+    mainImg.src = sImg[2].src;
+}
+sImg[3].onclick = function () {
+    mainImg.src = sImg[3].src;
+}
